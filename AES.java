06260c96a27753c41744e68b39b6c2a748437b4f
Cristian Solis <56800080+WhosDerpy@users.noreply.github.com>
2022-04-04 12:37:49 -0400

    deleting redundant files

--- AES.java
+++ AES.java
@@ -1,23 +1,32 @@
-import java.util.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 
-public class AES2 {
+public class AES {
+    private String yabe = "cNY1I3M05D7jyjNCv2NdFQ==";
+    private String eyeBee = "oLFQ3dPSDv02xD1S";
     private SecretKey key;
-    private Cipher encryptionCipher;
+    private byte[] IV;
 
     public void init() throws Exception {
         KeyGenerator generator = KeyGenerator.getInstance("AES");
-        generator.init(192);
+        generator.init(128);
         key = generator.generateKey();
+    }
+
+    public void initFromStrings() {
+        key = new SecretKeySpec(decode(yabe), "AES");
+        this.IV = decode(eyeBee);
     }
 
     public String encrypt(String message) throws Exception {
         byte[] messageInBytes = message.getBytes();
-        encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
-        encryptionCipher.init(Cipher.ENCRYPT_MODE, key);
+        Cipher encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        GCMParameterSpec spec = new GCMParameterSpec(128, IV);
+        encryptionCipher.init(Cipher.ENCRYPT_MODE, key, spec);
         byte[] encryptedBytes = encryptionCipher.doFinal(messageInBytes);
         return encode(encryptedBytes);
     }
@@ -29,7 +38,7 @@ public class AES2 {
     public String decrypt(String encryptedMessage) throws Exception {
         byte[] messageInBytes = decode(encryptedMessage);
         Cipher decryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
-        GCMParameterSpec spec = new GCMParameterSpec(128, encryptionCipher.getIV());
+        GCMParameterSpec spec = new GCMParameterSpec(128, IV);
         decryptionCipher.init(Cipher.DECRYPT_MODE, key, spec);
         byte[] decryptedBytes = decryptionCipher.doFinal(messageInBytes);
         return new String(decryptedBytes);
@@ -39,16 +48,21 @@ public class AES2 {
         return Base64.getDecoder().decode(data);
     }
 
+    private void exportKeys() {
+        System.err.println("SecretKey : " + encode(key.getEncoded()));
+        System.err.println("IV : " + encode(IV));
+    }
+
     public static void main(String[] args) {
         try {
-            AES2 aes = new AES2();
-            aes.init();
-            String encryptedMessage = aes.encrypt("");
+            AES aes = new AES();
+            aes.initFromStrings();
+            String encryptedMessage = aes.encrypt("da baby");
             String decryptedMessage = aes.decrypt(encryptedMessage);
 
             System.err.println("\nEncrypted message: " + encryptedMessage);
             System.err.println("Decrypted Message: " + decryptedMessage + "\n");
-
+            aes.exportKeys();
         } catch (Exception ignored) {
         }
     }
