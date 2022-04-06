c31cad5bb2f2248a37cc8c7bb5e3b541517ddd12
nalini1068 <naliniramdeo1068@gmail.com>
2022-04-06 15:14:38 -0400

    multithreaded version

---
+++ Myclient.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Myclient {
+
+    	
+	// driver code
+	public static void main(String[] args)
+	{
+		// establish a connection by providing host and port
+		// number
+		try (Socket socket = new Socket("localhost", 1234)) {
+			
+			// writing to server
+			PrintWriter out = new PrintWriter( //converts characters into bytes
+				socket.getOutputStream(), true);
+
+			// reading from server
+			BufferedReader in
+				= new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+
+			// object of scanner class
+			Scanner sc = new Scanner(System.in); //converts bytes into characters
+			String line = null;
+
+			while (!"exit".equalsIgnoreCase(line)) {
+				
+				// reading from user
+				line = sc.nextLine();
+
+				// sending the user input to server
+				out.println(line);
+				out.flush(); //clear the stream of any element 
+
+				// displaying server reply
+				System.out.println("Server replied "
+								+ in.readLine());
+			}
+			
+			// closing the scanner object
+			sc.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
