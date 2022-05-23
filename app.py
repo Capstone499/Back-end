41abbb1fe7a15cf3c5dcf2222819d5e0a4729c1d
juliannans <42010386+juliannans@users.noreply.github.com>
2022-05-23 12:43:39 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flask_pymongo import PyMongo 
 from flask_cors import CORS
-from pymongo import MongoClient
-import acc
+from acc import Mongo_URI
 from models import User
+#import bcrypt
 
 
 app=Flask(__name__,template_folder='templates')
 CORS(app)
+app.config['MONGO_URI'] = Mongo_URI
+mongo = PyMongo(app)
 
 
 @app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        self = mongo.db.self
+        return User.SignUp(self)
     return render_template("signup.html")
 
-#@app.route('/login', methods=["GET"])
-#def login():
-#    return render_template('login.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        self = mongo.db.self
+        return User.LogIn(self)
+    return render_template("login.html")
+
+@app.route("/landing", methods=["GET", "POST"])
+def success():
+    return render_template("landing.html")
+
+
+
+
 
 
--- models.py
+++ models.py
@@ -1,24 +1,47 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session, redirect, url_for
 import uuid
-import acc as db
+from acc import db
+
+
 
 class User:
-    def signing(self):
+
+
+    def SignUp(self):
+        print("Information sent to sign up:\t", request.form)
         user = {
             "_id": uuid.uuid4().hex,
-            "username": request.form.get('username'),
-            "password": request.form.get('password')
+            "username": request.form.get('username', type=str),
+            "password": request.form.get('password',  type=str)  
         }
-        db.userpass.insert_one(
+
+        if db.user_info.find_one({"username": user["username"]}):
+            return jsonify({"error": "Sorry, this username is already in use"}), 400
+        else:
+            db.user_info.insert_one(
                 {"_id": user["_id"],
                  "username": user["username"],
                  "password": user["password"]
-                 }
-            )
-        db.userinfo.insert_one(
-                 {"_id": user["_id"],
-                 "username": user["username"],
-                 "password": user["password"]
-                 }
-            )
-        return jsonify(user), 200
+                }
+            #user    
+        )
+        
+        return redirect(url_for('index'))
+
+    def LogIn(self):
+        print("Information sent to login:\t",request.form)
+        user = {
+            "username": request.form.get('user', type=str),
+            "password": request.form.get('pass', type=str)
+        }
+        validate = db.user_info.find_one({"username": user["username"]})
+        if(validate == None):
+            return jsonify({"error": "Wrong User"}), 400
+        else:
+            IsLoggedIn = True
+        if(validate["password"] == user["password"] and validate["username"] == user["username"]):
+                self.IsLoggedIn = True
+                return redirect(url_for('success'))
+                 
+        else:
+                return jsonify({"error": "Wrong User or Password"}), 400
